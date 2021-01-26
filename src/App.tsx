0b61d376f83d25c4d4948b27b4ac21aa84fdfb0a
aiden <campbell.e.aiden@gmail.com>
2021-01-26 08:15:10 +0100

    set up ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 /* global fetch */
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
@@ -21,9 +20,8 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{message}</p>
-        <p>Change me!</p>
+        <p>Its typescript & begin lets go !!!!!!!!!!!!! final test 123</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
